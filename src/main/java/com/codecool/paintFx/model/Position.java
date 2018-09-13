dd14da708fe762188d09cd90ad41a5814e8dd48e
adamGrob <ad4mgr0b@gmail.com>
2018-09-13 22:16:51 +0200

    Created the Position class for better coordinate management. (Like in the LinePositionController class.)

---
+++ src/main/java/com/codecool/paintFx/model/Position.java
@@ -0,0 +1,12 @@
+package com.codecool.paintFx.model;
+
+public class Position {
+
+
+    public double x, y;
+
+    public Position(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
