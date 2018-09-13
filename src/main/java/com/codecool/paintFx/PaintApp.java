69bfabe9953ce9acc7074ca9b24c70a37d2374fb
adamGrob <ad4mgr0b@gmail.com>
2018-09-13 18:33:38 +0200

    It is the main of the javafx Paint application.

---
+++ src/main/java/com/codecool/paintFx/PaintApp.java
@@ -0,0 +1,19 @@
+package com.codecool.paintFx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class PaintApp extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getClassLoader().getResource("paint.fxml"))));
+        stage.setTitle("Paint App");
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
