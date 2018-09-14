b68a9181528b49ab632401ed46f70fc828eb8c15
adamGrob <ad4mgr0b@gmail.com>
2018-09-14 22:28:32 +0200

    Added the display method, for smaller codebase, this class now can display himself.

--- src/main/java/com/codecool/paintFx/model/CustomLine.java
+++ src/main/java/com/codecool/paintFx/model/CustomLine.java
@@ -1,5 +1,7 @@
 package com.codecool.paintFx.model;
 
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Paint;
 import java.util.List;
 
 public class CustomLine extends MyShape {
@@ -10,11 +12,30 @@ public class CustomLine extends MyShape {
         this.straightLineList = straightLineList;
     }
 
-    public List<StraightLine> getStraightLineList() {
+    private List<StraightLine> getStraightLineList() {
         return straightLineList;
     }
 
     public void setStraightLineList(List<StraightLine> straightLineList) {
         this.straightLineList = straightLineList;
     }
+
+    @Override
+    public Paint getColor() {
+        return straightLineList.get(0).getColor();
+    }
+
+    @Override
+    public double getBrushSize() {
+        return straightLineList.get(0).getBrushSize();
+    }
+
+    @Override
+    public void display(GraphicsContext graphicsContext) {
+        for (StraightLine currStraightLine: getStraightLineList()) {
+            currStraightLine.display(graphicsContext);
+        }
+    }
+
+
 }
