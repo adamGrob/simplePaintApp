65b4c4ac46b67fb106d34ef433ca0f0eedd29ac7
adamGrob <ad4mgr0b@gmail.com>
2018-09-14 22:29:36 +0200

    Refactored by using inheritence. Removed the inherited methods. Added the display method for displaying.

--- src/main/java/com/codecool/paintFx/model/MyOval.java
+++ src/main/java/com/codecool/paintFx/model/MyOval.java
@@ -1,56 +1,19 @@
 package com.codecool.paintFx.model;
 
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Paint;
 
 public class MyOval extends MyShape{
-
-    private Paint color;
-
-    private double brushSize;
-
-    private double x, y;
 
     private double width, height;
 
     public MyOval(double x, double y, double width, double height, Paint color, double brushSize) {
         this.color = color;
         this.brushSize = brushSize;
-        this.x = x;
-        this.y = y;
+        this.startX = x;
+        this.startY = y;
         this.width = width;
         this.height = height;
-    }
-
-    public Paint getColor() {
-        return color;
-    }
-
-    public void setColor(Paint color) {
-        this.color = color;
-    }
-
-    public double getBrushSize() {
-        return brushSize;
-    }
-
-    public void setBrushSize(double brushSize) {
-        this.brushSize = brushSize;
-    }
-
-    public double getX() {
-        return x;
-    }
-
-    public void setX(double x) {
-        this.x = x;
-    }
-
-    public double getY() {
-        return y;
-    }
-
-    public void setY(double y) {
-        this.y = y;
     }
 
     public double getWidth() {
@@ -67,5 +30,10 @@ public class MyOval extends MyShape{
 
     public void setHeight(double height) {
         this.height = height;
+    }
+
+    @Override
+    public void display(GraphicsContext graphicsContext) {
+        graphicsContext.strokeOval(startX, startY, width, height);
     }
 }
