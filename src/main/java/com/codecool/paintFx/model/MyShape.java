8e1adbc611e6d32f9791a0190768d5e518473372
adamGrob <ad4mgr0b@gmail.com>
2018-09-14 22:31:19 +0200

    Added common methods, which will be inherited bu all the child classes.

--- src/main/java/com/codecool/paintFx/model/MyShape.java
+++ src/main/java/com/codecool/paintFx/model/MyShape.java
@@ -1,4 +1,48 @@
 package com.codecool.paintFx.model;
 
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Paint;
+
 public abstract class MyShape {
+
+    double startX, startY;
+
+    Paint color;
+
+    double brushSize;
+
+    public double getStartX() {
+        return startX;
+    }
+
+    public void setStartX(double startX) {
+        this.startX = startX;
+    }
+
+    public double getStartY() {
+        return startY;
+    }
+
+    public void setStartY(double startY) {
+        this.startY = startY;
+    }
+
+    public Paint getColor() {
+        return color;
+    }
+
+    public void setColor(Paint color) {
+        this.color = color;
+    }
+
+    public double getBrushSize() {
+        return brushSize;
+    }
+
+    public void setBrushSize(double brushSize) {
+        this.brushSize = brushSize;
+    }
+
+    public abstract void display(GraphicsContext graphicsContext);
+
 }
