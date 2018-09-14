886c45a9287aa4774965d19a1676c5eb98d6e9f7
adamGrob <ad4mgr0b@gmail.com>
2018-09-14 13:48:25 +0200

    Modified the PositionSnapper method, it is now correctly choosing the closest position in range to the snap the desired position into with the help of the new getClosestPosition method.

--- src/main/java/com/codecool/paintFx/controller/LinePositionController.java
+++ src/main/java/com/codecool/paintFx/controller/LinePositionController.java
@@ -3,33 +3,36 @@ package com.codecool.paintFx.controller;
 import com.codecool.paintFx.model.MyShape;
 import com.codecool.paintFx.model.StraightLine;
 import com.codecool.paintFx.model.Position;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class LinePositionController {
 
     public Position PositionSnapper (double xToSnap, double yToSnap, List<MyShape> shapeList, int rangeToSnap) {
-        double cordToSnapX = xToSnap;
-        double cordToSnapY = yToSnap;
+        Position positionToSnap = new Position(xToSnap, yToSnap);
+        List<Position> closestPositionList = new ArrayList<>();
         for (MyShape myShape: shapeList) {
             if(myShape.getClass() == StraightLine.class) {
                 StraightLine straightLine = (StraightLine)myShape;
-                double startPointX = straightLine.getStartX();
-                double startPointY = straightLine.getStartY();
-                double endPointX = straightLine.getEndX();
-                double endPointY = straightLine.getEndY();
-                if ((cordToSnapX <= startPointX + rangeToSnap && cordToSnapX >= startPointX - rangeToSnap) &&
-                        (cordToSnapY <= startPointY + rangeToSnap && cordToSnapY >= startPointY - rangeToSnap)) {
-                    xToSnap = startPointX;
-                    yToSnap = startPointY;
-                    return new Position(xToSnap, yToSnap);
-                } else if ((cordToSnapX <= endPointX + rangeToSnap && cordToSnapX >= endPointX - rangeToSnap) &&
-                        (cordToSnapY <= endPointY + rangeToSnap && cordToSnapY >= endPointY - rangeToSnap)) {
-                    xToSnap = endPointX;
-                    yToSnap = endPointY;
-                    return new Position(xToSnap, yToSnap);
-                }
+                closestPositionList.add(new Position(straightLine.getStartX(), straightLine.getStartY()));
+                closestPositionList.add(new Position(straightLine.getEndX(), straightLine.getEndY()));
             }
         }
-        return new Position(xToSnap, yToSnap);
+        return getClosestPosition(closestPositionList, positionToSnap, rangeToSnap);
+    }
+
+    private Position getClosestPosition(List<Position> positionList, Position referencePosition, int range) {
+        double smallestDistance = range;
+        Position closestPosition = referencePosition;
+        for (Position position: positionList) {
+            double currDistance = Math.sqrt((referencePosition.x - position.x) * (referencePosition.x - position.x) +
+                                    (referencePosition.y - position.y) * (referencePosition.y - position.y));
+            if(currDistance < smallestDistance) {
+               smallestDistance = currDistance;
+               closestPosition = position;
+            }
+        }
+        return closestPosition;
     }
 }
