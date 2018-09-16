1bb597fca00b7028d1c12afedcbb3958cdea489f
adamGrob <ad4mgr0b@gmail.com>
2018-09-16 12:20:55 +0200

    Created the MyPoligon abstarct class for the purpose of multiple inheritence among the modle classes, this helped applying the DRY principle more.

---
+++ src/main/java/com/codecool/paintFx/model/MyPoligon.java
@@ -0,0 +1,22 @@
+package com.codecool.paintFx.model;
+
+public abstract class MyPoligon extends MyShape{
+
+    double width, height;
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}
